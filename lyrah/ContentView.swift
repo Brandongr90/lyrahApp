c01969e1ad4663da7fbcb845fabb41dd767d4a86
Brandongr90 <l.gorb010920@itses.edu.mx>
2025-03-11 13:12:30 -0600

    Initial Commit

---
+++ lyrah/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lyrah
+//
+//  Created by Brandon Gonzalez on 11/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Lyrah!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lyrah/lyrahApp.swift
@@ -0,0 +1,17 @@
+//
+//  lyrahApp.swift
+//  lyrah
+//
+//  Created by Brandon Gonzalez on 11/03/25.
+//
+
+import SwiftUI
+
+@main
+struct lyrahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
